6973597f3d6bfb847e21ba15d0c44cf111dc121e
Benjamin David <ben@dukt.net>
2013-08-17 14:08:11 +0200

    Added actions/social/public/login controller

--- controllers/Social_PublicController.php
+++ controllers/Social_PublicController.php
@@ -2,9 +2,17 @@
 
 namespace Craft;
 
+require_once(CRAFT_PLUGINS_PATH.'social/vendor/autoload.php');
+
+use Guzzle\Http\Client;
+
 class Social_PublicController extends BaseController
 {
+	// --------------------------------------------------------------------
+
 	public $allowAnonymous = true;
+
+	// --------------------------------------------------------------------
 
 	public function actionLogout()
 	{
@@ -18,4 +26,39 @@ class Social_PublicController extends BaseController
 
 		$this->redirect($redirect);
 	}
-}
+
+	// --------------------------------------------------------------------
+
+	public function actionLogin()
+	{
+		// providerClass
+
+		$providerClass = craft()->request->getParam('provider');
+
+
+		// social callbackUrl
+
+        $callbackUrl = UrlHelper::getSiteUrl(craft()->config->get('actionTrigger').'/social/public/loginCallback');
+
+
+		// set session variables
+
+		craft()->oauth->httpSessionAdd('oauth.social', true);
+		craft()->oauth->httpSessionAdd('oauth.socialCallback', $callbackUrl);
+		craft()->oauth->httpSessionAdd('oauth.socialReferer', $_SERVER['HTTP_REFERER']);
+		craft()->oauth->httpSessionAdd('oauth.userMode', true);
+		craft()->oauth->httpSessionAdd('oauth.providerClass', $providerClass);
+
+
+		// connect to provider (with default scope)
+
+		$this->redirect(UrlHelper::getSiteUrl(
+				craft()->config->get('actionTrigger').'/oauth/public/connect',
+				array('provider' => $providerClass)
+			));
+
+		// ...
+
+		// script goes on in oauth/public/connect
+		// and then redirected to social/public/loginCallback
+	}
