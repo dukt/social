d12b25de1d88be7e6abe4704edc6bad892bc12d1
Benjamin David <ben@dukt.net>
2013-09-07 23:46:22 +0200

    0.9.8

--- SocialPlugin.php
+++ SocialPlugin.php
@@ -20,7 +20,7 @@ class SocialPlugin extends BasePlugin
      */
     function getVersion()
     {
-        return '0.9.7';
+        return '0.9.8';
     }
 
     // --------------------------------------------------------------------
