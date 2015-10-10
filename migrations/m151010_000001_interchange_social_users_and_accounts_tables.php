c3bcf416ccda2c0a3bfa7bce24b71d8fad18f95f
Benjamin David <ben@dukt.net>
2015-10-10 20:18:38 +0200

    Fixed migrations to get indexes/foreign keys renamed as table and column names change

--- migrations/m151010_000001_interchange_social_users_and_accounts_tables.php
+++ migrations/m151010_000001_interchange_social_users_and_accounts_tables.php
@@ -15,9 +15,9 @@ class m151010_000001_interchange_social_users_and_accounts_tables extends BaseMi
     {
         Craft::log('Interchanging social_users and social_accounts table names', LogLevel::Info, true);
 
-        $this->renameTable('social_users', 'social_accounts_temp');
-        $this->renameTable('social_accounts', 'social_users');
-        $this->renameTable('social_accounts_temp', 'social_accounts');
+        MigrationHelper::renameTable('social_users', 'social_accounts_temp');
+        MigrationHelper::renameTable('social_accounts', 'social_users');
+        MigrationHelper::renameTable('social_accounts_temp', 'social_accounts');
 
         Craft::log('Done interchanging social_users and social_accounts table names', LogLevel::Info, true);
 
---
+++ migrations/m151010_000002_rename_accounts_provider_column_by_gateway.php
@@ -0,0 +1,24 @@
+<?php
+namespace Craft;
+
+/**
+ * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_pluginHandle_migrationName
+ */
+class m151010_000002_rename_accounts_provider_column_by_gateway extends BaseMigration
+{
+    /**
+     * Any migration code in here is wrapped inside of a transaction.
+     *
+     * @return bool
+     */
+    public function safeUp()
+    {
+        Craft::log('Renaming social_accounts `provider` column by `gateway`', LogLevel::Info, true);
+
+        MigrationHelper::renameColumn('social_accounts', 'provider', 'gateway');
+
+        Craft::log('Done renaming social_accounts `provider` column by `gateway`', LogLevel::Info, true);
+
+        return true;
+    }
+}
