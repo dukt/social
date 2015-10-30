a23d645596915385ab141f7b23138a21159e45b9
Benjamin David <ben@dukt.net>
2015-10-30 02:32:38 +0100

    Added Social_ProvidersController

---
+++ controllers/Social_ProvidersController.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Social plugin for Craft
+ *
+ * @package   Craft Social
+ * @author    Benjamin David
+ * @copyright Copyright (c) 2015, Dukt
+ * @link      https://dukt.net/craft/social/
+ * @license   https://dukt.net/craft/social/docs/license
+ */
+
+namespace Craft;
+
+class Social_ProvidersController extends BaseController
+{
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * Settings
+     *
+     * @return null
+     */
+    public function actionIndex()
+    {
+        $variables['providers'] = craft()->social_providers->getProviders(false);
+
+        $this->renderTemplate('social/providers/_index', $variables);
+    }
+
+    /**
+     * Edit Provider
+     *
+     * @return null
+     */
+    public function actionEdit(array $variables = array())
+    {
+        if(!empty($variables['handle']))
+        {
+            $provider = craft()->social_providers->getProvider($variables['handle'], false, true);
+
+            if($provider)
+            {
+                $variables['infos'] = craft()->oauth->getProviderInfos($variables['handle']);;
+                $variables['provider'] = $provider;
+
+                $configInfos = craft()->config->get('providerInfos', 'oauth');
+
+                if(!empty($configInfos[$variables['handle']]))
+                {
+                    $variables['configInfos'] = $configInfos[$variables['handle']];
+                }
+
+                $this->renderTemplate('social/providers/_edit', $variables);
+            }
+            else
+            {
+                throw new HttpException(404);
+            }
+        }
+        else
+        {
+            throw new HttpException(404);
+        }
+    }
+}
