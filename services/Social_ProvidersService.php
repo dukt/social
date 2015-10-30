c2c10dd5c66e62815446037f6f4a22e20a8302a1
Benjamin David <ben@dukt.net>
2015-10-30 02:33:16 +0100

    Added Social_ProvidersService

---
+++ services/Social_ProvidersService.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Social plugin for Craft
+ *
+ * @package   Craft Social
+ * @author    Benjamin David
+ * @copyright Copyright (c) 2015, Dukt
+ * @link      https://dukt.net/craft/social/
+ * @license   https://dukt.net/craft/social/docs/license
+ */
+
+namespace Craft;
+
+class Social_ProvidersService extends BaseApplicationComponent
+{
+    // Public Methods
+    // =========================================================================
+
+    public function getProvider($handle, $configuredOnly = true)
+    {
+        $providers = $this->getProviders($configuredOnly);
+
+        foreach($providers as $provider)
+        {
+            if($provider->getHandle() == $handle)
+            {
+                return $provider;
+            }
+        }
+    }
+
+    public function getProviders($configuredOnly = true)
+    {
+        $providers = [];
+
+        $oauthProviders = craft()->oauth->getProviders($configuredOnly);
+        $oauthOptions = craft()->config->get('oauthOptions', 'social');
+
+        foreach($oauthProviders as $oauthProvider)
+        {
+            $provider = new Social_ProviderModel;
+            $provider->oauthProviderHandle = $oauthProvider->getHandle();
+            $customScope = null;
+
+            if(isset($oauthOptions[$provider->oauthProviderHandle]['scope']))
+            {
+                $customScope = $oauthOptions[$provider->oauthProviderHandle]['scope'];
+            }
+
+            $provider->customScope = $customScope;
+
+            $providers[] = $provider;
+        }
+
+        return $providers;
+    }
+}
