68e71eb2b18371a8c4ca053f474f723f2e109193
Andris Ševčenko <andris@pixelandtonic.com>
2016-03-01 22:31:34 +0200

    Clean up Services.

--- Source/social/services/Social_UserSessionService.php
+++ Source/social/services/Social_UserSessionService.php
@@ -27,7 +27,7 @@ class Social_UserSessionService extends UserSessionService
      * @var UserIdentity
      */
     private $_identity;
-    
+
     // Public Methods
     // =========================================================================
 
@@ -40,6 +40,13 @@ class Social_UserSessionService extends UserSessionService
         require_once(CRAFT_PLUGINS_PATH.'social/etc/Users/SocialUserIdentity.php');
     }
 
+    /**
+     * Login a user by social account ID.
+     * 
+     * @param $accountId
+     *
+     * @return bool
+     */
     public function login($accountId)
     {
         $rememberMe = true;
