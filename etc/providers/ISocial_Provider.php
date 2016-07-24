3e5b070fb839afd5ec0dab9b49540e2b574ac6c3
Benjamin David <ben@dukt.net>
2016-07-25 00:26:26 +0200

    PHP docs

--- etc/providers/ISocial_Provider.php
+++ etc/providers/ISocial_Provider.php
@@ -7,13 +7,25 @@ interface ISocial_Provider
 	// =========================================================================
 
 	/**
-	 * Get Name
+	 * Returns the name of the login provider
 	 *
 	 * @return string
 	 */
 	public function getName();
 
+	/**
+	 * Returns the handle of the OAuth provider
+	 *
+	 * @return string
+	 */
 	public function getOauthProviderHandle();
 
+	/**
+	 * Returns a profile from an OAuth token
+	 *
+	 * @param Oauth_TokenModel $token
+	 *
+	 * @return mixed
+	 */
 	public function getProfile(Oauth_TokenModel $token);
 }
