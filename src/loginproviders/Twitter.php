59c865f921a1818b531d58e1a0fb80358dba4c18
Benjamin David <ben@dukt.net>
2017-02-19 14:23:27 +0100

    Fixed Twitter

--- src/loginproviders/Twitter.php
+++ src/loginproviders/Twitter.php
@@ -7,25 +7,91 @@
 
 namespace dukt\social\loginproviders;
 
-use Craft\Craft;
+use Craft;
+use craft\helpers\UrlHelper;
 use dukt\social\models\Token;
 use dukt\social\Plugin as Social;
 
 class Twitter extends BaseProvider
 {
+    /**
+     * OAuth Connect
+     *
+     * @return null
+     */
+    public function oauthConnect()
+    {
+        // OAuth provider
+        $provider = $this->getOauthProvider();
+
+        // Obtain temporary credentials
+        $temporaryCredentials = $provider->getTemporaryCredentials();
+
+        // Store credentials in the session
+        Craft::$app->getSession()->set('oauth.temporaryCredentials', $temporaryCredentials);
+
+        // Redirect to login screen
+        $authorizationUrl = $provider->getAuthorizationUrl($temporaryCredentials);
+
+        return Craft::$app->getResponse()->redirect($authorizationUrl);
+    }
+
+    /**
+     * OAuth Callback
+     *
+     * @return null
+     */
+    public function oauthCallback()
+    {
+        $provider = $this->getOauthProvider();
+
+        $oauthToken = Craft::$app->request->getParam('oauth_token');
+        $oauthVerifier = Craft::$app->request->getParam('oauth_verifier');
+
+        // Retrieve the temporary credentials we saved before.
+        $temporaryCredentials = Craft::$app->getSession()->get('oauth.temporaryCredentials');
+
+        // Obtain token credentials from the server.
+        $token = $provider->getTokenCredentials($temporaryCredentials, $oauthToken, $oauthVerifier);
+
+        return [
+            'success' => true,
+            'token' => $token
+        ];
+    }
+
     public function getOauthProviderClass()
     {
         return '\League\OAuth1\Client\Server\Twitter';
     }
 
+    /**
+     * Get the OAuth provider.
+     *
+     * @return mixed
+     */
+    public function getOauthProvider()
+    {
+        $providerClass = $this->getOauthProviderClass();
+
+        $config = $this->getOauthProviderConfig();
+
+        if(!isset($config['callback_uri']))
+        {
+            $config['callback_uri'] = UrlHelper::actionUrl('social/oauth/callback');
+        }
+
+        return new $providerClass($config);
+    }
 
     public function getOauthProviderConfig()
     {
-        $providerInfos = Social::$plugin->oauth->getProviderInfos('google');
+        $providerInfos = Social::$plugin->oauth->getProviderInfos('twitter');
+        $oauthProviderOptions = $providerInfos['oauthProviderOptions'];
 
         $config = [
-            'identifier' => (isset($providerInfos['clientId']) ? $providerInfos['clientId'] : ''),
-            'secret' => (isset($providerInfos['clientSecret']) ? $providerInfos['clientSecret'] : ''),
+            'identifier' => (isset($oauthProviderOptions['consumerKey']) ? $oauthProviderOptions['consumerKey'] : ''),
+            'secret' => (isset($oauthProviderOptions['consumerSecret']) ? $oauthProviderOptions['consumerSecret'] : ''),
         ];
 
         return $config;
@@ -75,5 +141,17 @@ class Twitter extends BaseProvider
             'location' => $remoteProfile->location,
             'description' => $remoteProfile->description,
         ];
+    }
+
+    /**
+     * Returns the remote profile.
+     *
+     * @param $token
+     *
+     * @return array|null
+     */
+    public function getRemoteProfile(Token $token)
+    {
+        return $this->getOauthProvider()->getUserDetails($token->token);
     }
 }
