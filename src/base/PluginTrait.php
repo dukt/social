b429e702d68a541610335507a4ecc7f2e9b311a6
Benjamin David <ben@dukt.net>
2017-03-11 10:59:36 +0100

    Cleaned up `\dukt\social\base\PluginTrait`

--- src/base/PluginTrait.php
+++ src/base/PluginTrait.php
@@ -12,25 +12,11 @@ use dukt\social\Plugin as Social;
 /**
  * PluginTrait implements the common methods and properties for plugin classes.
  *
- * @property \dukt\social\services\Social           $social             The social service
  * @property \dukt\social\services\LoginAccounts    $loginAccounts      The loginAccounts service
  * @property \dukt\social\services\LoginProviders   $loginProviders     The loginProviders service
- * @property \dukt\social\services\Oauth            $oauth              The oauth service
- * @property \dukt\social\services\UserSession      $userSession        The userSession service
  */
 trait PluginTrait
 {
-    /**
-     * Returns the social service.
-     *
-     * @return \dukt\social\services\Social The social service
-     */
-    public function getSocial()
-    {
-        /** @var Social $this */
-        return $this->get('social');
-    }
-
     /**
      * Returns the loginAccounts service.
      *
@@ -51,27 +37,5 @@ trait PluginTrait
     {
         /** @var Social $this */
         return $this->get('loginProviders');
-    }
-
-    /**
-     * Returns the oauth service.
-     *
-     * @return \dukt\social\services\Oauth The oauth service
-     */
-    public function getOauth()
-    {
-        /** @var Social $this */
-        return $this->get('oauth');
-    }
-
-    /**
-     * Returns the userSession service.
-     *
-     * @return \dukt\social\services\UserSession The userSession service
-     */
-    public function getUserSession()
-    {
-        /** @var Social $this */
-        return $this->get('userSession');
     }
 }
