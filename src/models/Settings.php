90ff668f4fa5ad7c9e01c9a13702ebf4354ea5da
Benjamin David <ben@dukt.net>
2017-03-19 17:26:48 +0100

    Settings model

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -14,20 +14,38 @@ class Settings extends Model
     // Public Properties
     // =========================================================================
 
-    public $enableSocialRegistration;
-    public $enableSocialLogin;
+    /**
+     * @var bool Enable social registration
+     */
+    public $enableSocialRegistration = true;
+
+    /**
+     * @var bool Enable social login
+     */
+    public $enableSocialLogin = true;
+
+    /**
+     * @var mixed|null Login providers
+     */
     public $loginProviders;
+
+    /**
+     * @var int|null Default group
+     */
     public $defaultGroup;
-    public $autoFillProfile;
-    public $showCpSection;
-    public $enableCpLogin;
 
-    // Public Methods
-    // =========================================================================
+    /**
+     * @var bool Auto fill profile
+     */
+    public $autoFillProfile = true;
 
-    public function rules()
-    {
-        return [
-        ];
-    }
+    /**
+     * @var bool Show CP section
+     */
+    public $showCpSection = true;
+
+    /**
+     * @var bool Enable social login for the CP
+     */
+    public $enableCpLogin = false;
 }
