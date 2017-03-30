599ccdb6341314db9d4f7c72503afbb534553b05
Benjamin David <ben@dukt.net>
2017-03-30 16:42:07 +0200

    Reference

---
+++ reference/sami-config.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * Sami Documentation config
+ *
+*/
+
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->notName('sami.phar')
+    ->exclude('resources')
+    ->exclude('tests')
+    ->exclude('build')
+    ->exclude('cache')
+    ->exclude('migrations')
+    ->exclude('vendor')
+    ->exclude('templates')
+    ->in(__DIR__.'/../src');
+
+$options = array(
+    'title'                => 'Social for Craft CMS',
+    'build_dir'            => __DIR__.'/build',
+    'cache_dir'            => __DIR__.'/cache',
+    'default_opened_level' => 2
+);
+
+return new Sami($iterator, $options);
