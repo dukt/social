46b343ce53e066ce722e1401729c29bd7d6b60a9
Benjamin David <ben@dukt.net>
2017-09-05 15:58:45 +0200

    Docs

--- src/base/LoginProviderInterface.php
+++ src/base/LoginProviderInterface.php
@@ -9,6 +9,12 @@ namespace dukt\social\base;
 
 use dukt\social\models\Token;
 
+/**
+ * LoginProviderInterface defines the common interface to be implemented by login provider classes.
+ *
+ * @author Dukt <support@dukt.net>
+ * @since   1.0
+ */
 interface LoginProviderInterface
 {
     // Public Methods
