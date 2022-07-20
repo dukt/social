2d6b4e2165a68eb40ff199034a0f44d5106af494
Benjamin David <ben@dukt.net>
2022-07-20 22:55:53 +0200

    Fixed test title

--- tests/settings.spec.js
+++ tests/settings.spec.js
@@ -1,6 +1,6 @@
 const {test, expect} = require('@playwright/test');
 
-test('Shoud show the Settings page', async ({ page, context, baseURL }) => {
+test('Shoud show the login providers Settings page', async ({ page, context, baseURL }) => {
   await page.goto(baseURL + '/settings/social/loginproviders');
   const title = page.locator('h1');
   await expect(title).toHaveText('Social Settings');
